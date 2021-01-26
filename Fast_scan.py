53e53422bb34843e042caf40ede07685b0432ee4
LGOG <74514906+LGOG@users.noreply.github.com>
2021-01-26 15:59:28 -0500

    Add files via upload

---
+++ Fast_scan.py
@@ -0,0 +1,30 @@
+#!/usr/bin/pyhton
+
+import os
+
+
+print ("😵😵😵😵😵😵😵😵😵😵😵😵😵😵😵😵😵😵😵😵")
+
+IP = input("IP:? ")
+
+x = os.popen('rustscan -a '+IP).read()
+c = x.split()
+rust = c[153::]
+
+port_numbers = []
+
+#Takes only the ports
+a = [i for i,x in enumerate(rust) if x == "port"]
+for port in a:
+	port_numbers.append(rust[port+1])
+
+#takes only the numbers 
+open_ports = []
+for i in port_numbers:
+    open_ports.append(i.split('/')[0])
+
+
+#remove "" and spaces
+For_nmap = (','.join(open_ports))
+
+os.system ('nmap -A -p ' + (For_nmap) + " " +(IP) )
