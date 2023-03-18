2728dbc9e653c5f831a68c09bf01b96720c16979
LGOG <74514906+LGOG@users.noreply.github.com>
2023-03-18 16:16:20 -0400

    Update Fast_scan.py

--- Fast_scan.py
+++ Fast_scan.py
@@ -1,49 +1,37 @@
 #!/usr/bin/pyhton
-
+#Importing
 import os
-
+import re
 
 print ("😵😵😵😵😵😵😵😵😵😵😵😵😵😵😵😵😵😵😵😵")
 
-IP = input("IP:? ")
+#User input for IP
+IP = input("Which IP would you like me to scan? ")
 
 print ("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
+#Fast rustscan
 os.system('echo rustscan -a '+IP)
-rust_input = os.popen('rustscan -a '+IP).read()
-c = rust_input.split()
-rust = c[153::]
+rust_input = os.popen('rustscan --ulimit 5000 -a '+IP).read()
+
+#Takes only the ports
+port_list = re.findall(r'\d+/tcp', rust_input)
+port_list = [port.split('/')[0] for port in port_list]
+port_list = list(dict.fromkeys(port_list))
+ports = ','.join(port_list)
 
 print (rust_input)
 
-port_numbers = []
-
-#Takes only the ports
-a = [i for i,x in enumerate(rust) if x == "port"]
-for port in a:
-	port_numbers.append(rust[port+1])
-
-#Takes only the numbers 
-open_ports = []
-for i in port_numbers:
-    open_ports.append(i.split('/')[0])
-
-
-#Remove "" and spaces
-For_nmap = (','.join(open_ports))
-
 print ("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
+
+#Nmap deep scan
 #If you want to change the nmap options do it here
-os.system ('echo nmap -sV -A -sC -p ' + (For_nmap) + " " +(IP))
-
-os.system ('nmap -sV -A -sC -p ' + (For_nmap) + " " +(IP))
-
+os.system ('echo nmap -T4 -A -sC -p ' + (ports) + " " +(IP))
+os.system ('nmap -T4 -A -sC -p ' + (ports) + " " +(IP))
 print ("The ip was: " + IP)
+print (ports)
 
-print (For_nmap)
-ans = "no"
-web_port=" "
-
-for port in open_ports:
+#Web scan
+for port in port_list:
 	if port == str(80) or port == str(8080) or port == str(8081) or port == str(443) or port == str(8000):
 		print ("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
 		ans = input("Do you want to run gobuster?")
@@ -51,9 +39,9 @@ for port in open_ports:
 		if ans == "yes" or ans == "y" or ans == "YES" or ans == "Y":
 			print ("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
 			##If you want to change the gobuster options do it here
-			os.system('echo gobuster dir -w /usr/share/wordlists/dirb/common.txt -t 25 -x php,html,txt -q -u http://'+(IP)+":"+(web_port) )
-			os.system('gobuster dir -w /usr/share/wordlists/dirb/common.txt -t 25 -x php,html,txt -q -u http://'+(IP)+":"+(web_port) )
+			os.system('echo gobuster dir -w /usr/share/dirb/wordlists/common.txt -t 25 -x php,html,txt -q -u http://'+(IP)+":"+(web_port) )
+			os.system('gobuster dir -w /usr/share/dirb/wordlists/common.txt -t 25 -x php,html,txt -q -u http://'+(IP)+":"+(web_port) )
 		else: 
-			print ("Ok good luck! and have a good day! IP: "+ (IP))	
+			pass	
 	else:
-		pass
+		print ("Ok good luck! and have a good day! IP: "+ (IP)
