a2db2c2345489f95ea7c0027ff27d83bf5463930
LGOG <74514906+LGOG@users.noreply.github.com>
2024-04-10 11:49:00 -0400

    Update Fast_scan.py

--- Fast_scan.py
+++ Fast_scan.py
@@ -1,54 +1,73 @@
 import subprocess
 import argparse
 import re
+import signal
 
-# colorizing text
-YELLOW = '\033[93m'  # Yellow color
-GREEN = '\033[92m'   # Green color
-RESET = '\033[0m'    # Reset color to default
+# ANSI escape codes for colorizing text
+YELLOW = '\033[93m'
+GREEN = '\033[92m'
+RESET = '\033[0m'
+
+SQL_PORTS = {'3306': 'MySQL', '5432': 'PostgreSQL', '1433': 'MSSQL', '1521': 'Oracle'}
 
 def run_rustscan(ip_address):
-    # Runs RustScan on the specified IP address to quickly identify open ports.
+    """
+    Runs RustScan on the specified IP address to quickly identify open ports.
+    """
     print(f"{YELLOW}[~] Starting RustScan on {ip_address}...{RESET}")
     command = ['rustscan', '--ulimit', '5000', '-a', ip_address]
-    print(f"{GREEN}[>] {' '.join(command)}{RESET}")  # Print the full command syntax
+    print(f"{GREEN}[>] {' '.join(command)}{RESET}")
     result = subprocess.run(command, capture_output=True, text=True)
     print(result.stdout)
-    # Extract and return open port numbers from the RustScan output
     return re.findall(r'(\d+)/tcp', result.stdout)
 
 def run_nmap(ip_address, ports):
-    # Runs Nmap on the specified IP address and ports for detailed scanning.
+    """
+    Runs Nmap on the specified IP address and ports for detailed scanning.
+    """
     print(f"{YELLOW}[~] Starting Nmap deep scan on {ip_address} for ports {ports}...{RESET}")
     command = ['nmap', '-T4', '-sC', '-p', ports, ip_address]
-    print(f"{GREEN}[>] {' '.join(command)}{RESET}")  # Print the full command syntax
+    print(f"{GREEN}[>] {' '.join(command)}{RESET}")
     result = subprocess.run(command, capture_output=True, text=True)
     print(result.stdout)
     return result.stdout
 
 def find_web_ports(nmap_output):
-    #Parses Nmap output to find ports serving web content based on service names.
-    Considers any service containing 'http' as a web service.
-    web_services = re.findall(r'(\d+)/tcp open\s+([\w-]+)', nmap_output)
-    return [port for port, service in web_services if 'http' in service]
-    
+    """
+    Parses Nmap output to find ports serving web content based on service names.
+    """
+    web_services = re.findall(r'(\d+)/tcp\s+open\s+.*?http\b', nmap_output, re.IGNORECASE)
+    return [match.split('/')[0] for match in web_services]
+
 def run_gobuster(ip_address, port):
-    # Runs Gobuster on the specified IP address and port to enumerate directories,
-    after asking the user for confirmation.
+    """
+    Runs Gobuster on the specified IP address and port to enumerate directories.
+    """
     user_input = input(f"Do you want to run Gobuster on {ip_address}:{port}? (y/n): ")
     if user_input.lower() in ['y', 'yes']:
-        print(f"{YELLOW}[~] Starting Gobuster on {ip_address}:{port} with specified blacklist status codes...{RESET}")
+        print(f"{YELLOW}[~] Starting Gobuster on {ip_address}:{port}...{RESET}")
         url = f'http://{ip_address}:{port}'
         wordlist = '/usr/share/dirbuster/wordlists/directory-list-2.3-medium.txt'
         command = [
             'gobuster', 'dir', '-w', wordlist, '-t', '25', '--random-agent', '-r',
             '-b', '404,403,429,500,520', '-q', '-u', url
         ]
-        print(f"{GREEN}[>] {' '.join(command)}{RESET}")  # Print the full command syntax
+        print(f"{GREEN}[>] {' '.join(command)}{RESET}")
         subprocess.run(command)
 
+def run_sqlmap(ip_address, port):
+    """
+    Runs sqlmap on the specified IP address and port if an SQL service is detected.
+    """
+    print(f"{YELLOW}[~] Starting sqlmap on {ip_address}:{port}...{RESET}")
+    command = ['sqlmap', '-u', f'{ip_address}:{port}', '--batch']
+    print(f"{GREEN}[>] {' '.join(command)}{RESET}")
+    subprocess.run(command)
+
 def main():
-    # Main function that orchestrates the network scanning process.
+    """
+    Main function that orchestrates the network scanning process.
+    """
     parser = argparse.ArgumentParser(description='Fast Network Scanner')
     parser.add_argument('ip', help='IP address to scan')
     args = parser.parse_args()
@@ -61,11 +80,23 @@ def main():
         nmap_output = run_nmap(args.ip, ports_str)
         web_ports = find_web_ports(nmap_output)
 
-        for port in open_ports:  # Iterate over all open ports obtained from RustScan
-            if port in web_ports:
+        for port in web_ports:
+            try:
                 run_gobuster(args.ip, port)
+            except KeyboardInterrupt:
+                if port == web_ports[-1]:
+                    print(f"{YELLOW}This is the end of the scan.{RESET}")
+                    break
+                else:
+                    print(f"{YELLOW}[-] Gobuster scan interrupted. Moving to next port...{RESET}")
+                    continue
+
+        sql_ports = [port for port in open_ports if port in SQL_PORTS.keys()]
+        for port in sql_ports:
+            run_sqlmap(args.ip, port)
+
     else:
-        print("No ports identified by RustScan. Nmap and Gobuster scans will not be performed.")
+        print(f"{YELLOW}No ports identified by RustScan. Nmap and Gobuster scans will not be performed.{RESET}")
 
 if __name__ == '__main__':
     main()
